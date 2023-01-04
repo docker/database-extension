8ea81268a4c1b224e645b5da6b690f5d37bdf962
Benjamin Grandfond <benjamin.grandfond@docker.com>
2023-01-04 10:27:40 +0100

    Made some cleanup

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,27 +1,8 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import { createDockerDesktopClient } from "@docker/extension-api-client";
-import { Stack, TextField, Typography } from "@mui/material";
 import { Header } from "./components/Header";
 import { DatabasesCards } from "./components/DatabasesCards";
 
-// Note: This line relies on Docker Desktop's presence as a host application.
-// If you're running this React app in a browser, it won't work properly.
-const client = createDockerDesktopClient();
-
-function useDockerDesktopClient() {
-  return client;
-}
-
 export function App() {
-  const [response, setResponse] = React.useState<string>();
-  const ddClient = useDockerDesktopClient();
-
-  const fetchAndDisplayResponse = async () => {
-    const result = await ddClient.extension.vm?.service?.get("/hello");
-    setResponse(JSON.stringify(result));
-  };
-
   return (
     <>
       <Header />
